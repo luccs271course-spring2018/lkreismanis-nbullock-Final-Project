779ad3147e9f07f5f501516c90e25e343d4dc750
Larisa Kreismanis <lkreismanis@luc.edu>
2017-11-27 15:07:02 +0000

    UserPanel implementations and imports

--- src/UserPanel.java
+++ src/UserPanel.java
@@ -1,13 +1,18 @@
 /* 
  * File Name: UserPanel.java
  * Authors: Larisa Kreismanis, Nathan Bullock
- * Description: User panel of entire game.
+ * Description: User panel of Shape Shooter Arcade game.
  * Date Created: 11/17/17
 */
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.*;
 
-public class UserPanel {
+public class UserPanel extends JPanel implements KeyListener, ActionListener, MouseListener, ArcadeFriendly  {
+    
     
 } //end class
