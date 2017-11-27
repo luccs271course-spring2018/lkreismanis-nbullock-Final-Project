57d6bbf745aec1f902fe725c64442e21c2ab7cd5
Nathan Bullock <nbullock1@luc.edu>
2017-11-27 15:06:38 +0000

    Added Arcade and ArcadeFriendly

---
+++ src/Arcade.java
@@ -0,0 +1,20 @@
+/* 
+ * File Name: Arcade.java
+ * Authors: Larisa Kreismanis, Nathan Bullock
+ * Description: Panel used to display arcade game to the user
+ * Date Created: 11/27/17
+*/
+
+import java.awt.Container;
+import java.awt.BoarderLayout;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.EmptyBoarder;
+
+public class Arcade extends JFrame{
+    public Arcade(){
+        super("Shape Shooter Arcade"){
+            
+        }
+    }
+}
---
+++ src/ArcadeFriendly.java
@@ -0,0 +1,11 @@
+/* 
+ * File Name: ArcadeFriendly.java
+ * Authors: Larisa Kreismanis, Nathan Bullock
+ * Description: File makes other games accessible with this interface
+ * Date Created: 11/17/27
+*/
+
+public class ArcadeFriendly {
+    
+}
+
