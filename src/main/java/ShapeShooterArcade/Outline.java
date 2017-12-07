49beea88fec134bee37ea27a8612455a8b00756c
Larisa Kreismanis <lkreismanis@luc.edu>
2017-12-07 18:39:21 +0000

    Outline file completed

--- src/main/java/ShapeShooterArcade/Outline.java
+++ src/main/java/ShapeShooterArcade/Outline.java
@@ -6,6 +6,35 @@ package ShapeShooterArcade;
  * Date Created: 11/17/17
 */
 
+import java.awt.*;
+
 public class Outline {
-    return null;
+    private int locationX, locationY, id;
+    private boolean fill;
+    
+    public Outline(int x, int y, int i){
+        locationX = x;
+        locationY = y;
+        id = i;
+        fill = false;
+    }
+    public boolean getFill(){
+        return fill;
+    }
+    public boolean setFill(boolean t){
+        fill = t;
+    }
+    public int getX(){
+        return locationX;
+    }
+    public int getY(){
+        return locationY;
+    }
+    public int getId(){
+        return id;
+    }
+    
+    public abstract void draw(Graphics g);
+    
+    //return null;
 } //end class
